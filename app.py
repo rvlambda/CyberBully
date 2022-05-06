7303eb15c95b83313a80eb77a6e1458def8d15b9
rvlambda <59105388+rvlambda@users.noreply.github.com>
2022-05-06 11:47:43 +0530

    Adding to root

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import keras
+import joblib
+from sklearn.ensemble import RandomForestRegressor
+from prediction import get_prediction
+from keras.models import load_model
+
+model = load_model(r'Model/CyberBully_DL_Model.h5')
+
+st.set_page_config(page_title="CyberBully Sentiment Prediction",
+                   page_icon="🚧", layout="wide")
+
+
+st.markdown("<h1 style='text-align: center;'>CyberBully Sentiment Prediction 🚧</h1>", unsafe_allow_html=True)
+def main():
+    with st.form('prediction_form'):
+
+        st.subheader("Enter a tweet text to predict sentiment:")
+        text = st.text_input("text", value="", max_chars=100)
+        
+        submit = st.form_submit_button("Predict")
+
+
+    if submit:
+        data = text
+
+        pred = get_prediction(data=data, model=model)
+
+        st.write(f"The predicted sentiment for given text is:  {pred}")
+
+if __name__ == '__main__':
+    main()
